30fc3abe1dfb5045b7f58c83bc0a46f73de475aa
yibing <yibing.com>
2021-08-19 22:11:04 +0800

    1

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="control">
+  <div class="control-item" v-for="(item, index) in title" :key="index"
+   :class="{active: index === currentIndex}"
+   @click="tabItem(index)">
+     <span>{{item}}</span>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0
+    }
+  },
+  props: {
+    title: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  methods: {
+    tabItem(index) {
+      this.currentIndex = index
+    }
+  },
+}
+</script>
+
+<style>
+.control {
+  display: flex;
+  position: sticky;
+  top: 44px;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+.control-item {
+  flex: 1;
+}
+.active {
+  color: pink;
+}
+.active span {
+  padding: 5px;
+  border-bottom: 2px solid pink;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -6,21 +6,80 @@
   <home-swiper :banners="banners"></home-swiper>
   <recommend-view :recommends="recommends"></recommend-view>
   <feature-view/>
+  <tab-control :title="['流行','新款','精选']">
+
+  </tab-control>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+      <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+      <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+      <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+    <li>6</li>
+    <li>7</li>
+    <li>8</li>
+    <li>9</li>
+    <li>10</li>
+  </ul>
   </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from './childcomps/HomeSwiper.vue'
 import RecommendView from './childcomps/RecommendView.vue'
 import FeatureView from './childcomps/FeatureView.vue'
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from 'components/content/tabControl/TabControl.vue'
 import {getHomeMultidata} from 'network/home'
 export default {
   components: {
-    NavBar,
     HomeSwiper,
     RecommendView,
-    FeatureView
+    FeatureView,
+    NavBar,
+    TabControl,
+    TabControl
   },
   data() {
     return {
